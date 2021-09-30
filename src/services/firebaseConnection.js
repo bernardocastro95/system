0357d6e035f115002dbcb8c819ceaa208f6807f1
Bernardo Castro <bernardoc544@gmail.com>
2021-09-30 13:06:42 -0400

    Basic Structure

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/auth'
+
+let firebaseConfig = {
+
+    apiKey: "AIzaSyC_ijagu7PeLmGTNooeLVd5kP-vPCjShbY",
+  
+    authDomain: "system-e63be.firebaseapp.com",
+  
+    projectId: "system-e63be",
+  
+    storageBucket: "system-e63be.appspot.com",
+  
+    messagingSenderId: "685511986881",
+  
+    appId: "1:685511986881:web:5ac7ca35d66821c53b0e40",
+  
+    measurementId: "G-1GZ8G66993"
+  
+  };
+  
+ if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig)
+ }
+
+ export default firebase;
+  
+  
