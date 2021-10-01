9cfae846e5030dc9c453f681cfe7026461b10ac7
Bernardo Castro <bernardoc544@gmail.com>
2021-10-01 12:16:35 -0400

    Login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,32 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './login.css'
+import logo from '../../assets/logo.png'
+
 function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert('Click')
+  }
     return (
-      <div>
-        <h1>Login</h1>
+      <div className="container-center">
+        <div className="login">
+          <div className="logo-area">
+            <img src={logo} alt="system-logo"/>
+          </div>
+
+          <form onSubmit={handleSubmit}>
+            <h1>Enter</h1>
+            <input type="text" placeholder="mail@mail.com" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <input type="password" placeholder="********" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <button type="submit">Login</button>
+          </form>
+
+          <Link to="/register">Sign-Up</Link>
+        </div>
       </div>
     );
   }
