dc84bb7e3fdefd845f82af3a702d0bd1ebc4c649
Bernardo Castro <bernardoc544@gmail.com>
2021-11-26 12:14:42 -0500

    Upload Info

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -5,13 +5,91 @@ import Title from '../../components/Title'
 import {FiSettings, FiUpload} from 'react-icons/fi'
 import { AuthContext } from '../../contexts/auth'
 import avatar from '../../assets/avatar.png'
+import firebase from '../../services/firebaseConnection'
+import { toast } from 'react-toastify'
 
 export default function Profile(){
-    const {user, signOut} = useContext(AuthContext)
+    const {user, signOut, setUser, storageUser} = useContext(AuthContext)
 
     const [name, setName] = useState(user && user.name)
     const [email, setEmail] = useState(user && user.email)
     const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl)
+    const [imageAvatar, setImageAvatar] = useState(null)
+
+    function handleFile(e){
+        if(e.target.files[0]){
+            const img = e.target.files[0]
+
+            if(img.type === 'image/jpeg' || img.type === 'image/png'){
+                setImageAvatar(img)
+                setAvatarUrl(URL.createObjectURL(e.target.files[0]))
+            }
+            else {
+                toast.error('Image type not supported')
+                setImageAvatar(null)
+                return null
+            }
+        }
+    }
+
+    async function handleUpload(){
+        const current = user.uid
+
+        const uploadTask = await firebase.storage()
+        .ref(`images/${current}/${imageAvatar.name}`)
+        .put(imageAvatar)
+        .then(async ()=>{
+            toast.success('Photo Uploaded')
+
+            await firebase.storage().ref(`images/${current}`)
+            .child(imageAvatar.name).getDownloadURL()
+            .then(async (url)=>{
+                let urlPhoto = url
+
+                await firebase.firestore().collection('users')
+                .doc(user.uid)
+                .update({
+                    avatarUrl: urlPhoto,
+                    name: name
+                })
+                .then(()=> {
+                    let data = {
+                        ...user,
+                        avatarUrl: urlPhoto,
+                        name: name
+                    }
+                    setUser(data)
+                    storageUser(data)
+                })
+            })
+        })
+    }
+
+    async function handleSubmit(e){
+        e.preventDefault()
+        if(imageAvatar === null && name !== ''){
+            await firebase.firestore().collection('users')
+            .doc(user.uid)
+            .update({
+                name: name
+            })
+            .then(()=>{
+                let data = {
+                    ...user,
+                    name: name
+                }
+                setUser(data)
+                storageUser(data)
+                toast.success('Saved successfully')
+            })
+            .catch(()=>{
+
+            })
+        }
+        else if(name !== '' && imageAvatar !== null){
+            handleUpload()
+        }
+    }
     return(
         <div>
             <Header/>
@@ -22,13 +100,13 @@ export default function Profile(){
                 </Title>
 
                 <div className="container">
-                    <form className="form-profile">
+                    <form className="form-profile" onSubmit={handleSubmit}>
                         <label className="avatar">
                             <span>
                                 <FiUpload color="#fff" sizer={25}/>
                             </span>
 
-                            <input type="file" accept="image/*"/>
+                            <input type="file" accept="image/*" onChange={handleFile}/>
                             {avatarUrl === null ? 
                                 <img src={avatar} width="250" alt="Default Profile Picture"/>
                             : 
