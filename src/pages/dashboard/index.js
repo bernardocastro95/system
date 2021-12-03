2c67e467777183f4b507451df84c3f5d7be968a3
Bernardo Castro <bernardoc544@gmail.com>
2021-12-03 11:41:25 -0500

    Dashboard Styling

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,18 +1,45 @@
-import {useContext} from 'react'
-import {AuthContext} from '../../contexts/auth'
+import {useState} from 'react'
 import Header from '../../components/Header'
+import './dashboard.css'
+import Title from '../../components/Title'
+import {FiMessageSquare, FiPlus} from 'react-icons/fi'
+import {Link} from 'react-router-dom'
 
-function Dashboard() {
-  
-  const {signOut} = useContext(AuthContext)
+export default function Dashboard() {
+
+    const [calls, setCalls] = useState([])
+
     return (
       <div>
         <Header/>
-        <h1>Dashboard</h1>
-        <button onClick={()=> signOut()}>Sign Out</button>
+        <div className="content">
+          <Title name="Calls">
+            <FiMessageSquare size={25}/>
+          </Title>
+
+          {calls.length === 0 ? (
+               <div className="container dashboard">
+               <span>No call Registered...</span>
+   
+               <Link to="/new" className="new ">
+               <FiPlus sizer={25} color="#fff"/>
+                 New Call
+               </Link>
+             </div>
+          ): (
+            <>
+               <Link to="/new" className="new ">
+                <FiPlus sizer={25} color="#fff"/>
+                   New Call
+               </Link>
+            </>
+          )}
+
+       
+        </div>
+        
       </div>
     );
   }
   
-  export default Dashboard;
   
