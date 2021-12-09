96046767e17636264837b101a56f35ec21d689b0
Bernardo Castro <bernardoc544@gmail.com>
2021-12-09 12:56:37 -0500

    Functionalities for new call page

--- src/pages/new/index.js
+++ src/pages/new/index.js
@@ -2,10 +2,23 @@ import Header from '../../components/Header'
 import Title from '../../components/Title'
 import {FiPlusCircle} from 'react-icons/fi'
 import './new.css'
+import { useState } from 'react'
 export default function New(){
+
+    const [topic, setTopic] = useState('Support')
+    const [status, setStatus] = useState('open')
+    const [addOn, setAddOn] = useState('')
+
     function handleRegister(e){
         e.preventDefault()
         alert('TEST')
+    }
+
+    function handleChangeSelect(e){
+        setTopic(e.target.value)
+    }
+    function handleOptionChange(e){
+        setStatus(e.target.value)
     }
     return(
         <div>
@@ -25,7 +38,7 @@ export default function New(){
                             </option>
                         </select>
                         <label>Topic</label>
-                        <select>
+                        <select value= {topic} onChange={handleChangeSelect}>
                             <option value="Support">Support</option>
                             <option value="Technician">Technician</option>
                             <option value="Finance">Finance</option>
@@ -36,25 +49,33 @@ export default function New(){
                             type="radio"
                             name="radio"
                             value="open"
+                            onChange={handleOptionChange}
+                            checked={status === "open"}
                             />
                             <span>Opened</span>
                             <input 
                             type="radio"
                             name="radio"
                             value="progress"
+                            onChange={handleOptionChange}
+                            checked={status === "progress"}
                             />
                             <span>In Progress</span>
                             <input 
                             type="radio"
                             name="radio"
                             value="finished"
+                            onChange={handleOptionChange}
+                            checked={status === "finished"}
                             />
                             <span>Finished</span>
                         </div>
 
                         <label>Add-on</label>
                         <textarea type="text"
-                        placeholder="Describe your issue (optional)"/>
+                        placeholder="Describe your issue (optional)"
+                        value={addOn}
+                        onChange={(e) => setAddOn(e.target.value)}/>
 
                         <button type="submit">Save</button>
                     </form>
