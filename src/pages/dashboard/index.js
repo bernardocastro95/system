00748d570131be813b6c5b1dc543128709bda604
Bernardo Castro <bernardoc544@gmail.com>
2021-12-24 12:13:33 -0500

    Load Calls From Database

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,13 +1,101 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Header from '../../components/Header'
 import './dashboard.css'
 import Title from '../../components/Title'
 import {FiMessageSquare, FiPlus, FiSearch, FiEdit2} from 'react-icons/fi'
 import {Link} from 'react-router-dom'
+import firebase from '../../services/firebaseConnection'
+import {format} from 'date-fns'
 
+
+const listRef = firebase.firestore().collection('calls', 'desc').orderBy('created')
 export default function Dashboard() {
 
     const [calls, setCalls] = useState([])
+    const [load, setLoad] = useState(true)
+    const [loadingMore, setLoadingMore] = useState(false)
+    const [isEmpty, setIsEmpty] = useState(false)
+    const [lastDocs, setLastDocs] = useState()
+
+    useEffect(() => {
+
+      loadCalls()
+
+      return() => {
+
+      }
+    }, [])
+
+    async function loadCalls(){
+      await listRef.limit(5)
+      .get()
+      .then((snapshot)=>{
+        updateState(snapshot)
+      })
+      .catch((err)=>{
+        console.log(err)
+        setLoadingMore(false)
+      })
+
+      setLoad(false)
+    }
+
+    async function updateState(snapshot){
+      const isCollectionEmpty = snapshot.size === 0
+
+      if(!isCollectionEmpty){
+        let list = []
+
+        snapshot.forEach((doc) => {
+          list.push({
+            id: doc.id,
+            topic: doc.data().topic,
+            customer: doc.data().customer,
+            customerId: doc.data().customerId,
+            created: doc.data().created,
+            createdFormat: format(doc.data().created.toDate(), 'mm/dd/yyyy'),
+            status: doc.data().status,
+            addOn: doc.data().addOn
+          })
+        })
+
+        const lastDoc = snapshot.docs[snapshot.docs.length - 1]
+
+        setCalls(calls => [...calls, ...list])
+        setLastDocs(lastDoc)
+      }
+      else {
+        setIsEmpty(true)
+      }
+      setLoadingMore(false)
+    }
+
+    async function handleMore(){
+      setLoadingMore(true)
+      await listRef.startAfter(lastDocs).limit(5)
+      .get()
+      .then((snapshot) => {
+        updateState(snapshot)
+      })
+
+    }
+
+    if(load){
+      return(
+        <div>
+          <Header/>
+            <div className="content">
+              <Title name="Calls">
+                <FiMessageSquare size={25}/>
+              </Title>
+
+              <div className='container dashboard'>
+                <span>Searching for calls...</span>
+              </div>
+            </div>
+        </div>
+      )
+    }
 
     return (
       <div>
@@ -44,24 +132,30 @@ export default function Dashboard() {
                    </tr>
                  </thead>
                  <tbody>
-                 <tr>
-                   <td data-label="Customer">Subject</td>
-                   <td data-label="Topic">Support</td>
-                   <td data-label="Status">
-                     <span className="badge" style={{backgroundColor: '#5cb85c'}}>Current</span>
-                   </td>
-                   <td data-label="Registered">12/09/2021</td>
-                   <td data-label="#">
-                     <button className="action" style={{backgroundColor: '#3583f6'}}>
-                       <FiSearch color="#FFF" size={17}/>
-                     </button>
-                     <button className="action" style={{backgroundColor: '#f6a935'}}>
-                       <FiEdit2 color="#FFF" size={17}/>
-                     </button>
-                   </td>
-                 </tr>
+                   {calls.map((item, index) => {
+                     return(
+                      <tr key={index}>
+                      <td data-label="Customer">{item.customer}</td>
+                      <td data-label="Topic">{item.topic}</td>
+                      <td data-label="Status">
+                        <span className="badge" style={{backgroundColor: item.status === "open" ? '#5cb85c' : '#999'}}>{item.status}</span>
+                      </td>
+                      <td data-label="Registered">{item.createdFormat}</td>
+                      <td data-label="#">
+                        <button className="action" style={{backgroundColor: '#3583f6'}}>
+                          <FiSearch color="#FFF" size={17}/>
+                        </button>
+                        <button className="action" style={{backgroundColor: '#f6a935'}}>
+                          <FiEdit2 color="#FFF" size={17}/>
+                        </button>
+                      </td>
+                    </tr>
+                     )
+                   })}       
                </tbody>
               </table>
+              {loadingMore && <h3 style={{textAlign: 'center', marginTop: 15}}>Searching Data...</h3>}
+              {!loadingMore && !isEmpty && <button className='btn-more' onClick={handleMore}>More Calls</button>}
                
             </>
           )}
